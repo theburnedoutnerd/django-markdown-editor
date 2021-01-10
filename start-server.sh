0645c017e6bd5712b8b8442bdf7fe10785354b14
Alissa McGee <6390646+theburnedoutnerd@users.noreply.github.com>
2021-01-10 15:02:20 -0600

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,5 @@
-if [ -n "amcge" ] && [ -n "devpass9" ] ; then
+# start-server.sh
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd martor_demo; python manage.py createsuperuser --no-input)
 fi
 (cd martor_demo; gunicorn martor_demo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
